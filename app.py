78454480aeb0b17c09a910d00d32ca2d6bfc035e
Tobi <mrt_s_jackson@instance-1.europe-west2-c.c.core-almanac-283307.internal>
2020-07-23 12:01:07 +0000

    templates!

--- app.py
+++ app.py
@@ -1,34 +1,5 @@
-from flask import Flask
-  
-app = Flask(__name__)
-@app.route('/')
-@app.route('/home')
-def home():
-    return """
-    <html>
-  <head>
-    <title>Tobi's flask</title>
-    </head>
-    <body>
-        <h1>The Adventures of QA</h1>
-        <div id="mainContent">
-          <p>My QA has a lot of adventures. Yesterday we <a href="about">Made a Flask
-</a>, and i made two!</p>
-          <p>Here's not a picture of QA:</p>
-        </div>
-         <div id="sidebar">
-             <h2>Buy our stuff!</h2>
-             <p>Some of our products include <span class="product">SuperWidgets</span
->, <span class="product">MegaWidgets</span>, and <span class="product">WonderWidgets<
-/span>.</p>
-         </div>
-     </body>
-    </html> 
-    """
-@app.route('/')
-@app.route('/about')
-def about():
-    return """
-    <title>"Im about"</title>
-    <body> "Guess who this about"</body>
-    """
+# imports app object from ./application/__init__.py
+from application import app
+# if the file is being run directly, and not imported, then start the application. 
+if __name__ == '__main__':
+ app.run(debug=True, host='0.0.0.0')
