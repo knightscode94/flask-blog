19cdf99b26014fa500c6d36fb559053e15488ecb
Tobi <mrt_s_jackson@instance-1.europe-west2-c.c.core-almanac-283307.internal>
2020-07-23 11:06:31 +0000

    correct i hope

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
-
+  
 app = Flask(__name__)
-
 @app.route('/')
 @app.route('/home')
 def home():
@@ -13,17 +12,19 @@ def home():
     <body>
         <h1>The Adventures of QA</h1>
         <div id="mainContent">
-          <p>My QA has a lot of adventures. Yesterday we <a href="about">Made a Flask</a>, and i made two!</p>
+          <p>My QA has a lot of adventures. Yesterday we <a href="about">Made a Flask
+</a>, and i made two!</p>
           <p>Here's not a picture of QA:</p>
         </div>
          <div id="sidebar">
              <h2>Buy our stuff!</h2>
-             <p>Some of our products include <span class="product">SuperWidgets</span>, <span class="product">MegaWidgets</span>, and <span class="product">WonderWidgets</span>.</p>
+             <p>Some of our products include <span class="product">SuperWidgets</span
+>, <span class="product">MegaWidgets</span>, and <span class="product">WonderWidgets<
+/span>.</p>
          </div>
      </body>
     </html> 
     """
-
 @app.route('/')
 @app.route('/about')
 def about():
