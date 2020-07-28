749bff9810091705f4ee6d6d8836318207c9cc3c
Tobi <mrt_s_jackson@instance-1.europe-west2-c.c.core-almanac-283307.internal>
2020-07-28 11:08:00 +0000

    update data

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from flask_login import login_user, current_user, logout_user, login_required
-from application.forms import PostForm, RegistrationForm, LoginForm
+from application.forms import PostForm, RegistrationForm, LoginForm, UpdateAccountForm
 from flask import render_template, redirect, url_for, request
 from application import app, db, bcrypt
 from application.models import Posts, Users
@@ -33,14 +33,18 @@ def about():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     if form.validate_on_submit():
         hash_pw = bcrypt.generate_password_hash(form.password.data)
-
-        user = Users(email=form.email.data, password=hash_pw)
-
+        
+        user = Users(first_name=form.first_name.data,
+                last_name=form.last_name.data,
+                email=form.email.data,
+                password=hash_pw)
+    
         db.session.add(user)
         db.session.commit()
-
         return redirect(url_for('post'))
     return render_template('register.html', title='Register', form=form)
 
@@ -50,20 +54,16 @@ def post():
     form = PostForm()
     if form.validate_on_submit():
         postData = Posts(
-            first_name = form.first_name.data,
-            last_name = form.last_name.data,
             title = form.title.data,
-            content = form.content.data
-        )
+            content = form.content.data,
+            author = current_user
+            )
 
         db.session.add(postData)
         db.session.commit()
-
         return redirect(url_for('home'))
-
     else:
         print(form.errors)
-
     return render_template('post.html', title='Post', form=form)
 
 @app.route("/logout")
@@ -71,3 +71,19 @@ def post():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.first_name = form.first_name.data
+        current_user.last_name = form.last_name.data
+        current_user.email = form.email.data
+        db.session.commit()
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.first_name.data = current_user.first_name
+        form.last_name.data = current_user.last_name
+        form.email.data = current_user.email
+    return render_template('account.html', title='Account', form=form)
