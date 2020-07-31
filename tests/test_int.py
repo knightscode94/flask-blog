7d7b550209e714b3aa218357f41646f9e98dee2e
Tobi <mrt_s_jackson@instance-1.europe-west2-c.c.core-almanac-283307.internal>
2020-07-31 07:51:46 +0000

    working intergration tests!

--- tests/test_int.py
+++ tests/test_int.py
@@ -8,7 +8,7 @@ from flask_testing import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from application import app, db, bcrypt
-from application.models import Users
+from application.models import Users, Posts
 
 test_admin_first_name = "admin"
 test_admin_last_name = "admin"
@@ -24,11 +24,11 @@ class TestBase(LiveServerTestCase):
 
     def setUp(self):
         """Setup the test driver and create test users"""
-        print("--------------------------NEXT-TEST----------------------------------------------")
         chrome_options = Options()
         chrome_options.binary_location = "/usr/bin/chromium-browser"
         chrome_options.add_argument("--headless")
-        self.driver = webdriver.Chrome(executable_path="/home/mrt_s_jackson/chromedriver", chrome_options=chrome_options)
+        self.driver = webdriver.Chrome(executable_path="/home/mrt_s_jackson/chromedriver",
+                chrome_options=chrome_options)
         self.driver.get("http://localhost:5000")
         db.session.commit()
         db.drop_all()
@@ -36,11 +36,16 @@ class TestBase(LiveServerTestCase):
 
     def tearDown(self):
         self.driver.quit()
-        print("--------------------------END-OF-TEST----------------------------------------------\n\n\n-------------------------UNIT-AND-SELENIUM-TESTS----------------------------------------------")
+
+#########################################################################################
+
+class TestRunning(TestBase):
 
     def test_server_is_up_and_running(self):
         response = urlopen("http://localhost:5000")
         self.assertEqual(response.code, 200)
+
+class TestReg(TestBase):
 
     def test_registration(self):
         """
@@ -48,8 +53,8 @@ class TestBase(LiveServerTestCase):
         if all fields are filled out correctly, and that they will be 
         redirected to the login page
         """
-# Click register menu link
-        self.driver.find_element_by_xpath('/html/body/strong/nav/ul/a[3]').click()
+        # Click register menu link
+        self.driver.find_element_by_xpath('/html/body/a[3]').click()
         time.sleep(1)
 
         # Fill in registration form
@@ -68,5 +73,3 @@ class TestBase(LiveServerTestCase):
         # Assert that browser redirects to login page
         assert url_for('login') in self.driver.current_url
 
-if __name__ == '__main__':
-    unittest.main(port=5000)
